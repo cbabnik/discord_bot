9239444153b7d777226b70a42dbfdcbddf0fc52f
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-01 21:59:07 -0800

    Put constants in their own file

---
+++ constants.js
@@ -0,0 +1,8 @@
+exports.MAX_MESSAGES = 1000;
+exports.PERMISSIONS = 36838464;  // find this via the developer portal -> bot -> permissions calculator
+exports.DISCORD_CHANNEL = 123;
+
+exports.CONFIG_DEFAULTS = {
+    LOG_DIRECTORY: "./logs",
+    MAIN_CHANNEL: "shitposting"
+};
