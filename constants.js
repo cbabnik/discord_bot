de3b4d8a0db64f0c311ce8af40c060d890ef0e96
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-02 22:59:47 -0800

    Update constants to use channel ID

--- constants.js
+++ constants.js
@@ -1,8 +1,7 @@
 exports.MAX_MESSAGES = 1000;
 exports.PERMISSIONS = 36838464;  // find this via the developer portal -> bot -> permissions calculator
-exports.DISCORD_CHANNEL = 123;
 
 exports.CONFIG_DEFAULTS = {
     LOG_DIRECTORY: "./logs",
-    MAIN_CHANNEL: "shitposting"
+    MAIN_CHANNEL: "265430059010097162"
 };
