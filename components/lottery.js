b381902dbad01bb3a59983e3dfb35da45b0d08e0
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-03 10:48:17 -0800

    Simplify lottery component

--- components/lottery.js
+++ components/lottery.js
@@ -5,26 +5,21 @@ const ID = "lottery";
 class Lottery extends Component {
     constructor() {
         super(ID);
-        this.addCommand("\\+win$", this.roll);
+        this.addCommand("\\+win$", this.win);
     }
 
-    roll(metaInfo) {
+    win(metaInfo) {
         const {author} = metaInfo;
-        if (this.json[author] === undefined)
-            this.json[author] = 0;
-        const winsAmount = this.json[author]+1;
-        this.json[author] = winsAmount;
-        this.setAction("message", "You win! This was your " + winsAmount + "th win.");
+        const winsAmt = this.json[author]+1 || 1;
+        this.json[author] = winsAmt;
         this.saveJSON();
+        this.setAction("message", "You win! This was your " + winsAmt +
+            (winsAmt%10===1?"st":winsAmt%10===2?"nd":winsAmt%10===3?"rd":"th") + " win.");
     }
 
     getAmt(author) {
-        const winsAmount = this.json[author];
-        if (winsAmount !== undefined)
-            return winsAmount;
+        return this.json[author] || 0;
     }
 }
 
-const lottery = new Lottery();
-
-module.exports = { lottery };
+module.exports = { lottery: new Lottery() };
