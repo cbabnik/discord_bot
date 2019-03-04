dd7543e3630c46e435cd99390b7aadbb624b0976
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-03 19:45:24 -0800

    Add main voice channel to constants

--- constants.js
+++ constants.js
@@ -3,5 +3,6 @@ exports.PERMISSIONS = 36838464;  // find this via the developer portal -> bot ->
 
 exports.CONFIG_DEFAULTS = {
     LOG_DIRECTORY: "./logs",
-    MAIN_CHANNEL: "265430059010097162"
+    MAIN_CHANNEL: "265430059010097162",
+    MAIN_VOICE_CHANNEL: "265430059500699648",
 };
