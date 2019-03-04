2241641e056affcc24400e0d2f02a260f49b2089
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-04 00:39:33 -0800

    Add some test framework

---
+++ test/example.js
@@ -0,0 +1,43 @@
+const { example } = require('../components/example');
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+// stub saveJSON so nothing is written
+
+// beforeEach set json to something to ensure predictability
+
+describe("Example", () => {
+    describe('+win', () => {
+        it('should count 1st then 2nd', () => {
+
+        });
+        it('should not crash if the json file is empty', () => {
+
+        });
+        it('should track wins independently from different sources', () => {
+
+        });
+    });
+
+    describe('+delay', () => {
+        it('', () => {
+
+        });
+    });
+    describe('+spam', () => {
+        it('', () => {
+
+        });
+    });
+    describe('+countdown', () => {
+        it('', () => {
+
+        });
+    });
+    describe('+alarm', () => {
+        it('', () => {
+
+        });
+    });
+});
