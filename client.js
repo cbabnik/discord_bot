d1acf113831d011a36273b26971a4e2034ff3c22
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-10 16:15:51 -0700

    Disconnect from voice channels after a few seconds of inactivity

--- client.js
+++ client.js
@@ -11,14 +11,31 @@ const Client = (max_messages, login_token) => {
 
     // try to login every 60 seconds if disconnected
     cli.login(login_token).then( () => cli.setInterval(
-        async () => {
-            if (cli.status !== Discord.isConnected)
-                await cli.login(login_token)
+        async function tryLogin() {
+            try {
+                if (cli.status !== Discord.isConnected)
+                    await cli.login(login_token)
+            }
+            catch (err) {
+                console.log("Client error: " + err.message);
+            }
         },
         60000
     ));
 
+    let voice_activity = {};
+    // after ~3 seconds of inactivity disconnect from voice channels
+    cli.setInterval( () => {
+        cli.voiceConnections.array().forEach((c) => {
+            vc = c.channel;
+            n = vc.name;
 
+            const streamCount = c.player.streamingData.count;
+            if (streamCount === voice_activity[n])
+                vc.leave();
+            voice_activity[n] = streamCount;
+        })
+    }, 3000);
     return cli;
 };
 
