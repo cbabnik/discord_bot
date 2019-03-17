d9cf0f09fb139aab09ea558ea83197feb9107592
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-17 16:55:17 -0700

    [tests] Eslint utility tests

--- test/utility.js
+++ test/utility.js
@@ -3,46 +3,46 @@ const { utility } = require('../components/utility');
 const expect = require('chai').expect;
 const sinon = require('sinon');
 
-const saveStub = sinon.stub(utility, "saveJSON");
+sinon.stub(utility, 'saveJSON');
 
-describe("Utility", () => {
+describe('Utility', () => {
 
     describe('-random', () => {
         it('check comma seperated values', () => {
-            utility.random("a,b,c");
+            utility.random('a,b,c');
             const result = utility.commitAction();
-            expect(["a", "b", "c"]).to.include(result.message);
+            expect(['a', 'b', 'c']).to.include(result.message);
         });
         it('check space seperated values', () => {
-            utility.random("a b c");
+            utility.random('a b c');
             const result = utility.commitAction();
-            expect(["a", "b", "c"]).to.include(result.message);
+            expect(['a', 'b', 'c']).to.include(result.message);
         });
         it('check that spaces take priority', () => {
-            utility.random("a,b c,d");
+            utility.random('a,b c,d');
             const result = utility.commitAction();
-            expect(["a,b", "c,d"]).to.include(result.message);
+            expect(['a,b', 'c,d']).to.include(result.message);
         });
     });
 
     describe('-roll', () => {
         it('check negative values', () => {
-            utility.roll("-5", "-5");
+            utility.roll('-5', '-5');
             const result = utility.commitAction();
-            expect(result.message).to.equal("-5");
+            expect(result.message).to.equal('-5');
         });
         it('check big values', () => {
-            utility.roll("10000000000000000000000", "10000000000000000000000");
+            utility.roll('10000000000000000000000', '10000000000000000000000');
             const result = utility.commitAction();
-            expect(result.message).to.equal("10000000000000000000000");
+            expect(result.message).to.equal('10000000000000000000000');
         });
     });
 
     describe('-math', () => {
         it('evaluates complicated expressions', () => {
-            utility.calculate("((5**2 + 100)/5)**.5/.5");
+            utility.calculate('((5**2 + 100)/5)**.5/.5');
             const result = utility.commitAction();
-            expect(result.message).to.include("10");
+            expect(result.message).to.include('10');
         });
     });
 
@@ -56,17 +56,17 @@ describe("Utility", () => {
         });
         it('heads works', () => {
             sandbox.stub(Math, 'random').returns(0.5);
-            utility.coinflip("heads", "tails");
+            utility.coinflip('heads', 'tails');
             const result = utility.commitAction();
-            expect(result.message).to.include("heads");
-            expect(result.image).to.include("heads");
+            expect(result.message).to.include('heads');
+            expect(result.image).to.include('heads');
         });
         it('tails works', () => {
             sandbox.stub(Math, 'random').returns(0.0);
-            utility.coinflip("heads", "tails");
+            utility.coinflip('heads', 'tails');
             const result = utility.commitAction();
-            expect(result.message).to.include("tails");
-            expect(result.image).to.include("tails");
+            expect(result.message).to.include('tails');
+            expect(result.image).to.include('tails');
         });
     });
 });
