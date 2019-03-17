11c69e6d309da55b22c0cfc1744f070017ff1057
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-16 17:18:35 -0700

    Add tests for utility

---
+++ test/utility.js
@@ -0,0 +1,40 @@
+const { utility } = require('../components/utility');
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const saveStub = sinon.stub(utility, "saveJSON");
+
+describe("Utility", () => {
+
+    describe('+random', () => {
+        it('check comma seperated values', () => {
+            utility.random("a,b,c");
+            const result = utility.commitAction();
+            expect(["a", "b", "c"]).to.include(result.message);
+        });
+        it('check space seperated values', () => {
+            utility.random("a b c");
+            const result = utility.commitAction();
+            expect(["a", "b", "c"]).to.include(result.message);
+        });
+        it('check that spaces take priority', () => {
+            utility.random("a,b c,d");
+            const result = utility.commitAction();
+            expect(["a,b", "c,d"]).to.include(result.message);
+        });
+    });
+
+    describe('+roll', () => {
+        it('check negative values', () => {
+            utility.roll("-5", "-5");
+            const result = utility.commitAction();
+            expect(result.message).to.equal("-5");
+        });
+        it('check big values', () => {
+            utility.roll("10000000000000000000000", "10000000000000000000000");
+            const result = utility.commitAction();
+            expect(result.message).to.equal("10000000000000000000000");
+        });
+    });
+});
