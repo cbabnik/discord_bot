3116b4ce417ec47f7b863b482ad5e70ee9df5b6b
Curtis Babnik <curtisbabnik@gmail.com>
2019-03-24 12:57:09 -0700

    Register secret commands whenever they exist

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const { Client } = require('./client');
 const { DispatcherGenerator } = require('./dispatch');
 const { Monitor } = require('./monitor');
@@ -16,3 +18,7 @@ dispatcher.registerComponent(require('./components/utility').utility);
 dispatcher.registerComponent(require('./components/help').help);
 dispatcher.registerComponent(require('./components/audio').audio);
 dispatcher.registerComponent(require('./components/pictures').pictures);
+
+if (fs.existsSync('./components/secret.js')) {
+    dispatcher.registerComponent(require('./components/secret').secret);
+}
