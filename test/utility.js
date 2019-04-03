915576bfc789ef1a111831f640263532beaa15b8
Curtis Babnik <curtisbabnik@gmail.com>
2019-04-03 14:08:35 -0700

    Eslint test files

--- test/utility.js
+++ test/utility.js
@@ -1,72 +1,72 @@
-const { utility } = require('../components/utility');
+const { utility } = require( '../components/utility' );
 
-const expect = require('chai').expect;
-const sinon = require('sinon');
+const expect = require( 'chai' ).expect;
+const sinon = require( 'sinon' );
 
-sinon.stub(utility, 'saveJSON');
+sinon.stub( utility, 'saveJSON' );
 
-describe('Utility', () => {
+describe( 'Utility', () => {
 
-    describe('-random', () => {
-        it('check comma seperated values', () => {
-            utility.random('a,b,c');
+    describe( '-random', () => {
+        it( 'check comma seperated values', () => {
+            utility.random( 'a,b,c' );
             const result = utility.commitAction();
-            expect(['a', 'b', 'c']).to.include(result.message);
-        });
-        it('check space seperated values', () => {
-            utility.random('a b c');
+            expect( ['a', 'b', 'c'] ).to.include( result.message );
+        } );
+        it( 'check space seperated values', () => {
+            utility.random( 'a b c' );
             const result = utility.commitAction();
-            expect(['a', 'b', 'c']).to.include(result.message);
-        });
-        it('check that spaces take priority', () => {
-            utility.random('a,b c,d');
+            expect( ['a', 'b', 'c'] ).to.include( result.message );
+        } );
+        it( 'check that spaces take priority', () => {
+            utility.random( 'a,b c,d' );
             const result = utility.commitAction();
-            expect(['a,b', 'c,d']).to.include(result.message);
-        });
-    });
+            expect( ['a,b', 'c,d'] ).to.include( result.message );
+        } );
+    } );
 
-    describe('-roll', () => {
-        it('check negative values', () => {
-            utility.roll('-5', '-5');
+    describe( '-roll', () => {
+        it( 'check negative values', () => {
+            utility.roll( '-5', '-5' );
             const result = utility.commitAction();
-            expect(result.message).to.equal('-5');
-        });
-        it('check big values', () => {
-            utility.roll('10000000000000000000000', '10000000000000000000000');
+            expect( result.message ).to.equal( '-5' );
+        } );
+        it( 'check big values', () => {
+            utility.roll( '10000000000000000000000', '10000000000000000000000' );
             const result = utility.commitAction();
-            expect(result.message).to.equal('10000000000000000000000');
-        });
-    });
+            expect( result.message ).to.equal( '10000000000000000000000' );
+        } );
+    } );
 
-    describe('-math', () => {
-        it('evaluates complicated expressions', () => {
-            utility.calculate('((5**2 + 100)/5)**.5/.5');
+    describe( '-math', () => {
+        it( 'evaluates complicated expressions', () => {
+            utility.calculate( '((5**2 + 100)/5)**.5/.5' );
             const result = utility.commitAction();
-            expect(result.message).to.include('10');
-        });
-    });
+            expect( result.message ).to.include( '10' );
+        } );
+    } );
 
-    describe('-coinflip', () => {
+    describe( '-coinflip', () => {
         let sandbox;
-        beforeEach(() => {
+        beforeEach( () => {
             sandbox = sinon.createSandbox();
-        });
-        afterEach(() => {
+        } );
+        afterEach( () => {
             sandbox.restore();
-        });
-        it('heads works', () => {
-            sandbox.stub(Math, 'random').returns(0.5);
-            utility.coinflip('heads', 'tails');
+        } );
+        it( 'heads works', () => {
+            sandbox.stub( Math, 'random' ).returns( 0.5 );
+            utility.coinflip( 'heads', 'tails' );
             const result = utility.commitAction();
-            expect(result.message).to.include('heads');
-            expect(result.image).to.include('heads');
-        });
-        it('tails works', () => {
-            sandbox.stub(Math, 'random').returns(0.0);
-            utility.coinflip('heads', 'tails');
+            expect( result.message ).to.include( 'heads' );
+            expect( result.image ).to.include( 'heads' );
+        } );
+        it( 'tails works', () => {
+            sandbox.stub( Math, 'random' ).returns( 0.0 );
+            utility.coinflip( 'heads', 'tails' );
             const result = utility.commitAction();
-            expect(result.message).to.include('tails');
-            expect(result.image).to.include('tails');
-        });
-    });
-});
+            expect( result.message ).to.include( 'tails' );
+            expect( result.image ).to.include( 'tails' );
+        } );
+    } );
+} );
