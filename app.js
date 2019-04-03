e9bb04674c42e4698ca73a40e4916cd6591b6292
Curtis Babnik <curtisbabnik@gmail.com>
2019-04-03 14:12:16 -0700

    Plug in all the new components

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-const fs = require('fs');
+const fs = require( 'fs' );
 
-const { Client } = require('./client');
-const { DispatcherGenerator } = require('./dispatch');
-const { Monitor } = require('./monitor');
-const { Scanner } = require('./scan');
-const { Actor } = require('./actor');
-const { LOGIN_TOKEN } = require('./auth');
-const { MAX_MESSAGES } = require('./constants');
+const { Client } = require( './client' );
+const { DispatcherGenerator } = require( './dispatch' );
+const { Monitor } = require( './monitor' );
+const { Scanner } = require( './scan' );
+const { Actor } = require( './actor' );
+const { LOGIN_TOKEN } = require( './auth' );
+const { MAX_MESSAGES } = require( './constants' );
 
-const client = Client(MAX_MESSAGES, LOGIN_TOKEN);
-const actor = Actor(client);
+const client = Client( MAX_MESSAGES, LOGIN_TOKEN );
+const actor = Actor( client );
 const dispatcher = DispatcherGenerator( Scanner )( actor );
-Monitor(client, dispatcher);
+Monitor( client, dispatcher );
 
-dispatcher.registerComponent(require('./components/utility').utility);
-dispatcher.registerComponent(require('./components/help').help);
-dispatcher.registerComponent(require('./components/audio').audio);
-dispatcher.registerComponent(require('./components/pictures').pictures);
-dispatcher.registerComponent(require('./components/lottery').lottery);
+dispatcher.registerComponent( require( './components/utility' ).utility );
+dispatcher.registerComponent( require( './components/help' ).help );
+dispatcher.registerComponent( require( './components/audio' ).audio );
+dispatcher.registerComponent( require( './components/pictures' ).pictures );
+dispatcher.registerComponent( require( './components/lottery' ).lottery );
+dispatcher.registerComponent( require( './components/bank' ).bank );
+dispatcher.registerComponent( require( './components/admin' ).admin );
 
-if (fs.existsSync('./components/secret.js')) {
-    dispatcher.registerComponent(require('./components/secret').secret);
+if ( fs.existsSync( './components/secret.js' ) ) {
+    dispatcher.registerComponent( require( './components/secret' ).secret );
 }
+
+const { payroll } = require( './components/payroll' );
+dispatcher.registerComponent( payroll );
+
+// client needs some time to setup, so we'll just give it a second.
+setTimeout( () => {
+    payroll.bootUp( actor );
+}, 1000 );
