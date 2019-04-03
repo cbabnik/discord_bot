87576e0d7a559fe1d42a00006c09f5e5e7f8a872
Curtis Babnik <curtisbabnik@gmail.com>
2019-04-03 14:04:28 -0700

    Make Eslint more scrutinous

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,17 @@ module.exports = {
         ],
         "keyword-spacing": [
             "error"
+        ],
+        "no-constant-condition": [
+            "off"
+        ],
+        "space-in-parens": [
+            "error",
+            "always",
+            { "exceptions": ["empty"] }
+        ],
+        "prefer-const": [
+            "error"
         ]
     }
 };
