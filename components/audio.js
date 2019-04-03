15cb4e5e13f12da078eb0af00d64684b5112a201
Curtis Babnik <curtisbabnik@gmail.com>
2019-04-03 14:12:01 -0700

    Eslint components

--- components/audio.js
+++ components/audio.js
@@ -1,34 +1,43 @@
-const { Component } = require('../component');
-const _ = require('lodash');
-const fs = require('fs');
+const { Component } = require( '../component' );
+const _ = require( 'lodash' );
+const fs = require( 'fs' );
 
 const ID = 'audio';
 
 class Audio extends Component {
     constructor() {
-        super(ID);
-        this.addCommand(/^-play random$/, this.playRandom);
-        this.addCommand(/^!random$/, this.playRandom);
-        this.addCommand(/^-play ([^/\\]+)$/, this.playAudio);
-        this.addCommand(/^-end$/, this.endAudio);
-        this.addCommand(/^!([^/\\]+)$/, this.playAudio);
+        super( ID );
+        this.addCommand( /^-play random$/, this.playRandom );
+        this.addCommand( /^!random$/, this.playRandom );
+        this.addCommand( /^-endAudio$/, this.endAudio );
+        this.addCommand( /^!end$/, this.endAudio );
+        this.addCommand( /^-play ([^/\\]+)$/, this.playAudio );
+        this.addCommand( /^!([^/\\]+)$/, this.playAudio );
+        this.addCommand( /^-play (.+)$/, this.playYoutube );
+        this.addCommand( /^!(.+)$/, this.playYoutube );
     }
 
-    playAudio(fileName) {
-        if (fileName.includes('youtube') && fileName.includes('http')) {
-            this.setAction('audioYoutube', fileName);
+    playAudio( fileName ) {
+        this.setAction( 'audioFile', fileName );
+    }
+
+    playYoutube( url ) {
+        if ( url.includes( 'youtube' ) || url.includes( 'youtu.be' ) ) {
+            this.setAction( 'audioYoutube', url ) ;
+        } else if ( url.includes( 'http' ) ) {
+            this.setAction( 'audioLink', url ) ;
         } else {
-            this.setAction('audioFile', fileName);
+            this.setAction( 'message', 'Try a file name or a youtube url' );
         }
     }
 
     endAudio() {
-        this.setAction('endAudio', true);
+        this.setAction( 'endAudio', true );
     }
 
     playRandom() {
-        const f = _.sample(fs.readdirSync('./audio'));
-        this.setAction('audioFile', f);
+        const f = _.sample( fs.readdirSync( './audio' ) );
+        this.setAction( 'audioFile', f );
     }
 }
 
--- components/utility.js
+++ components/utility.js
@@ -1,73 +1,87 @@
-const { Component } = require('../component');
+const { Component } = require( '../component' );
+const _ = require( 'lodash' );
 
-const bigInt = require('big-integer');
+const bigInt = require( 'big-integer' );
 
 const ID = 'utility';
 
 class Utility extends Component {
     constructor() {
-        super(ID);
-        this.addCommand(/^-roll (\d+)$/, (max) => this.roll(1,max));
-        this.addCommand(/^-roll (-?\d+) (-?\d+)$/, this.roll);
-        this.addCommand(/^-roll/, this.rollInfo);
-        this.addCommand(/^-random (\S+(?: \S+)+)$/, this.random);
-        this.addCommand(/^-random (\S+(?:,[^\s,]+)+)$/, this.random);
-        this.addCommand(/^-random \S+$/, this.randomInfoB);
-        this.addCommand(/^-random/, this.randomInfo);
-        this.addCommand(/^-math ([-+*/.()\d]*)$/, this.calculate);
-        this.addCommand(/^-math/, this.calculateInfo);
-        this.addCommand(/^-coinflip$/, () => this.coinflip('', ''));
-        this.addCommand(/^-coinflip (\S+) (\S+)$/, this.coinflip);
+        super( ID );
+        this.addCommand( /^-roll (\d+)$/, ( max ) => this.roll( 1,max ) );
+        this.addCommand( /^-roll (-?\d+) (-?\d+)$/, this.roll );
+        this.addCommand( /^-roll/, this.rollInfo );
+        this.addCommand( /^-random pick(\d+) (\S+(?: \S+)+)$/, this.random );
+        this.addCommand( /^-random pick(\d+) (\S+(?:,[^\s,]+)+)$/, this.random );
+        this.addCommand( /^-random pick (\d+) (\S+(?: \S+)+)$/, this.random );
+        this.addCommand( /^-random pick (\d+) (\S+(?:,[^\s,]+)+)$/, this.random );
+        this.addCommand( /^-random (\S+(?:,[^\s,]+)+)$/, ( list ) => this.random( 1,list ) );
+        this.addCommand( /^-random (\S+(?: \S+)+)$/, ( list ) => this.random( 1,list ) );
+        this.addCommand( /^-random \S+$/, this.randomInfoB );
+        this.addCommand( /^-random/, this.randomInfo );
+        this.addCommand( /^-math ([ \-+*/.()\d]*)$/, this.calculate );
+        this.addCommand( /^-coinflip$/, () => this.coinflip( '', '' ) );
+        this.addCommand( /^-coinflip (\S+) (\S+)$/, this.coinflip );
+        this.addCommand( /^-coinflip/, this.coinflipInfo );
     }
 
     rollInfo() {
-        this.setAction('message', 'Try `-roll [min] [max]`');
+        this.setAction( 'message', 'Try `-roll [min] [max]`' );
     }
 
-    roll(min, max) {
-        const result = bigInt.randBetween(min,max);
-        this.setAction('message', result.toString());
+    roll( min, max ) {
+        const result = bigInt.randBetween( min,max );
+        this.setAction( 'message', result.toString() );
     }
 
     randomInfo() {
-        this.setAction('message', 'Try `-random [a] [b] [c]`');
+        this.setAction( 'message', 'Try `-random [a] [b] [c]`' );
     }
     randomInfoB() {
-        this.setAction('message', 'You need more than one option to random between');
+        this.setAction( 'message', 'You need more than one option to random between' );
     }
 
-    random(options) {
-        if (options.includes(' ')) {
-            options = options.split(' ');
+    random( n, options ) {
+        if ( options.includes( ' ' ) ) {
+            options = options.split( ' ' );
         } else {
-            options = options.split(',');
+            options = options.split( ',' );
         }
-        const index = Math.floor(Math.random()*options.length);
-        this.setAction('message', options[index]);
-    }
-
-    calculateInfo() {
-        this.setAction('message', 'Invalid use, try `?math` to learn more.');
+        n = Number( n );
+        if ( n === 1 ) {
+            this.setAction( 'message', _.sample( options ) );
+        } else if ( n === 0 ) {
+            this.setAction( 'message', '(What were you expecting?)' );
+            this.setAction( 'delay', 3 );
+        } else if ( n > options.length ) {
+            this.setAction( 'message', 'number has to be less than or equal to the number of elements to random between.' );
+        } else {
+            this.setAction( 'message', _.sampleSize( options, n ).join( ' ' ) );
+        }
     }
 
     // WARNING: be very careful to validate input for this function
-    calculate(str) {
+    calculate( str ) {
         try {
-            const result = eval(str);
-            this.setAction('message', 'The result is: ' + result.toString());
-        } catch (_) {
-            this.setAction('message', 'No value could be determined.');
+            const result = eval( str );
+            this.setAction( 'message', 'The result is: ' + result.toString() );
+        } catch ( _ ) {
+            this.setAction( 'message', 'No value could be determined.' );
         }
     }
 
-    coinflip(a, b) {
-        if (Math.random() >= 0.5) {
-            this.setAction('message', a);
-            this.setAction('image', 'heads.jpg');
+    coinflip( a, b ) {
+        if ( Math.random() >= 0.5 ) {
+            this.setAction( 'message', a );
+            this.setAction( 'image', 'heads.jpg' );
         } else {
-            this.setAction('message', b);
-            this.setAction('image', 'tails.jpg');
+            this.setAction( 'message', b );
+            this.setAction( 'image', 'tails.jpg' );
         }
+    }
+
+    coinflipInfo() {
+        this.setAction( 'message', 'Invalid use, try `-coinflip HEADS TAILS`' );
     }
 }
 
