a68e7fa59b153a05fc8fe6aa25fd4437cd3d5442
Curtis Babnik <curtisbabnik@gmail.com>
2019-04-03 18:12:56 -0700

    Add -give command to give away credits

--- components/bank.js
+++ components/bank.js
@@ -1,6 +1,7 @@
 const { Component } = require( '../component' );
 const _ = require( 'lodash' );
 const debug = require( 'debug' )( 'basic' );
+const { BUCKS } = require( '../constants' );
 
 const ID = 'bank';
 
@@ -8,6 +9,8 @@ class Bank extends Component {
     constructor() {
         super( ID );
         this.addCommand( /^-balance$/, this.getAmount );
+        this.addCommand( /^-give (.+) (\d+)$/, this.give );
+        this.addCommand( /^-give (.+) (-\d+)$/, this.stealWarning );
     }
 
     getAmount( metaInfo ) {
@@ -17,7 +20,7 @@ class Bank extends Component {
         if ( amount === 0 ) {
             this.setAction( 'message', `Sorry **${user}**, you're flat out broke.` );
         } else if ( amount < 0 ) {
-            this.setAction( 'message', `Dang **${user}**, you're in debt! You owe slots ${-amount} credits. You're lucky I havn't implemented savage interest rates yet.` );
+            this.setAction( 'message', `Dang **${user}**, you're in debt! You owe slots ${-amount} credits.` );
         } else {
             this.setAction( 'message', `**${user}** has **${amount}** credits banked.` );
         }
@@ -55,6 +58,25 @@ class Bank extends Component {
         _.set( this.json, `${id}.${type}`, balance - value );
         this.saveJSON();
         return true;
+    }
+
+    give(user, amnt, metaInfo) {
+        const id = BUCKS[player.toUpperCase()];
+        if (!id) {
+            this.setAction('message', `Sorry, I could not find user **${user}**`);
+            return;
+        }
+
+        if (this.payAmount(metaInfo.authorId, amnt)) {
+            this.addAmount(id, amnt);
+            this.setAction('message', `**${metaInfo.author}** has given ${amnt} credits to **${user}**`);
+        } else {
+            this.setAction('message', `**${metaInfo.author}**, you don't have enough credits. Check with \`-balance\``);
+        }
+    }
+
+    stealWarning() {
+        this.setAction('message', 'Are you trying to steal? shame on you.');
     }
 }
 
