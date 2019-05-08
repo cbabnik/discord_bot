49df823b912da3cc2a14b02cdc9d744fc5c2bad4
Curtis Babnik <curtisbabnik@gmail.com>
2019-05-07 22:29:06 -0700

    Upgrade component with timed events and helper functions

--- component.js
+++ component.js
@@ -14,6 +14,11 @@
 
 const fs = require( 'fs' );
 const { CONFIG_DEFAULTS } = require( './constants' );
+const debug = require( 'debug' )( 'basic' );
+const util = require( './util' );
+const _ = require( 'lodash' );
+
+const DAYMS = 1000*60*60*24;
 
 class Component {
     constructor( id ) {
@@ -23,6 +28,10 @@ class Component {
         this.action = {};
         this.actionPart = this.action;
         this.commands = [];
+
+        setTimeout(() => {
+            this.bootUp();
+        }, 3000);
     }
 
     addCommand( regex, cb ) {
@@ -55,6 +64,72 @@ class Component {
             fs.mkdir( CONFIG_DEFAULTS.STORAGE_DIRECTORY, {}, () => {} );
         }
         fs.writeFile( this.jsonFile, JSON.stringify( this.json ), 'utf8', () => {} );
+    }
+
+    get(field, default_val=0) {
+        _.set(this.json, field, default_val);
+    }
+
+    set(field, value) {
+        _.set(this.json, field, value);
+    }
+
+    update(field, operator, default_val=0, f = _.sum) {
+        _.set(this.json, field, f( _.get(this.json, field, default_val) , operator) );
+    }
+
+    bootUp() {
+        // to override if needed.
+        // This triggers 3 seconds after constructor to allow several things to set up before actions are made
+    }
+
+    addScheduledEvent(time=util.time.today(), field='timestamps.default', delay=DAYMS) {
+        // TODO: add repeat = false option to scheduled events, or some manner of unscheduling
+        if (typeof time !== 'number') {
+            time = time.getTime()
+        }
+        const currentTime = new Date().getTime();
+        let lastTime;
+        if (currentTime <= time) { // if passed time
+            lastTime = time + Math.floor((currentTime-time)/delay)*delay;
+        }
+        else {
+            lastTime = time - Math.ceil((time-currentTime)/delay)*delay;
+        }
+        const nextTime = lastTime+delay;
+        const timestamp = this.get(field, undefined);
+        if ( typeof timestamp === 'undefined' ) {
+            this.scheduledEvent(0, field);
+            this.set(field, lastTime);
+        }
+        const misses =  Math.floor((currentTime - timestamp)/delay);
+        if (misses > 0) {
+            this.set(field, lastTime);
+            this.scheduledEvent(misses, field);
+        }
+        setTimeout( () => {
+            this.scheduledEvent(0, field);
+            setInterval( () => {
+                this.set(field, lastTime);
+            }, delay)
+        }, nextTime)
+    }
+
+    scheduledEvent(misses, field) {
+        // to override if needed. Should trigger at 12:00:30
+    }
+
+    setActor(actor) {
+        // some components need to proactively fire off actions
+        this.actor = actor;
+    }
+
+    bypassDispatcher() {
+        if (typeof this.actor === 'undefined') {
+            debug('You can\'t bypass the dispatcher without setting the actor');
+        }
+        const instructions = this.commitAction();
+        this.actor.handle(instructions, null)
     }
 }
 
